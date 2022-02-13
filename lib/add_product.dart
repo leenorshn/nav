be6fa2ea22fdbc4979fd1d2c6b158c3204666697
leenorshn <katembovictor@gmail.com>
2022-02-13 10:43:23 +0200

    form fiel

--- lib/add_product.dart
+++ lib/add_product.dart
@@ -1,34 +1,88 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:nav/main_page.dart';
 
-class AddProduct extends StatelessWidget {
+class AddProduct extends StatefulWidget {
   const AddProduct({Key? key}) : super(key: key);
 
+  @override
+  State<AddProduct> createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  num taille = 0;
+  num age = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("New prod"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Nouveau produit",
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+        children: [
+          TextFormField(
+            onChanged: (v) {
+              setState(() {
+                taille = num.parse(v);
+              });
+            },
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              label: const Text(" Votre taille"),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("Retour en arriere"),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          TextFormField(
+            onChanged: (v) {
+              setState(() {
+                age = num.parse(v);
+              });
+            },
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              label: const Text("votre Age"),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              //suffixIcon: const Icon(CupertinoIcons.eye),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 64,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onPressed: () {
+              if (taille > 1.6) {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => const MainPage()));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                        " Vous avez ${age} ans et de $taille cm de taille"),
+                    duration: const Duration(seconds: 6),
+                    backgroundColor: Colors.orange,
+                  ),
+                );
+              }
+            },
+            child: const Text("submit"),
+          ),
+        ],
       ),
     );
   }
